ce663e65d0b19af1d1114e6b9db9599e72fece76
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-08-10 21:50:24 +0300

    Update Header.h

--- Bank_Account/Header.h
+++ Bank_Account/Header.h
@@ -9,10 +9,10 @@ private:
     std::string acctnum;//Client Number Type string.
     double balance;//Balance.
 public:
-    BankAccount(const std::string& client,
+    BankAccount(const std::string& client,//constructer.
         const std::string& num, double bal = 0.0);
-    void show(void) const;
-    void deposit(double cash);
+    void show(void) const;//the show function.
+    void deposit(double cash);//function for the deposit opreation.
     void withdraw(double cash);
 };
 
