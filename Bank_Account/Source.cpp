0055097977ab00fc76eabbaac588de288780deeb
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-08-21 22:56:11 +0300

    Update Source.cpp

--- Bank_Account/Source.cpp
+++ Bank_Account/Source.cpp
@@ -1,25 +1,25 @@
 #include"Header.h"
 #include <iostream>
 
-BankAccount::BankAccount(const std::string& client, const std::string& num, double bal)//initialize the class.
+BankAccount::BankAccount(const std::string& client, const std::string& num, double bal)//initialize the class
 {
-    name = client;//link them with the main class.
-    acctnum = num;//same here.
-    if (bal < 0)//checking if the balance is in positive or not.
+    name = client;//link them with the main class
+    acctnum = num;//same here
+    if (bal < 0)//checking if the balance is in positive or not
     {
         std::cout << "Number of ballance can't be negative; balance set to 0.\n";
         balance = .0;
     }
     else
-        balance = bal;//link them with the main class.
+        balance = bal;//link them with the main class
 }
-void BankAccount::show(void) const// Show the values.
+void BankAccount::show(void) const// Show the values
 {
     std::cout << "Name:   " << name
         << "\naccount: " << acctnum
         << "\nbalance: " << balance << std::endl;
 }
-void BankAccount::deposit(double cash)// deposit.
+void BankAccount::deposit(double cash)// deposit
 {
     if (cash < 0)
         std::cout << "Number of deposit can't be negative. "
