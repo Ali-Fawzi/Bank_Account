806556c07bddff7708e2413e18318b7bd8959de1
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-08-28 19:45:51 +0300

    Update Main.cpp

--- Bank_Account/Main.cpp
+++ Bank_Account/Main.cpp
@@ -1,7 +1,7 @@
 #include"Header.h"
 
 int main() {
-	BankAccount Aizen{ "Aizen","998",10000 };//we can add anthing other the these
+	BankAccount Aizen{ "Aizen","998",10000 };//we can add anthing other the these.
 	Aizen.show();//Showing the fuction
 	Aizen.withdraw(20000);//To check if everthing is okay.
 	Aizen.show();//show function.
