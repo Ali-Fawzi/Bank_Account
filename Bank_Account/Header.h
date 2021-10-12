7804e635973faf67f4741620e7bec46f52af3973
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-10-12 21:03:08 +0300

    Update Header.h

--- Bank_Account/Header.h
+++ Bank_Account/Header.h
@@ -5,15 +5,15 @@
 class BankAccount
 {
 private:
-    std::string name;//Client Name.
-    std::string acctnum;//Client Number Type string.
-    double balance;//Balance.
+    std::string name;//Client Name
+    std::string acctnum;//Client Number Type string
+    double balance;//Balance
 public:
-    BankAccount(const std::string& client,//constructer.
+    BankAccount(const std::string& client,//constructer
         const std::string& num, double bal = 0.0);
-    void show(void) const;//the show function.
-    void deposit(double cash);//function for the deposit opreation.
-    void withdraw(double cash);//function for the withdraw opreation.
+    void show(void) const;//the show function
+    void deposit(double cash);//function for the deposit opreation
+    void withdraw(double cash);//function for the withdraw opreation
 };
 
 #endif // !HEADER
